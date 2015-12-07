629cd0d633d9c974f5aaa6e0ea2823e4d6a0d2d5
Eugenio Mendoza Robaina <eu.mendoza@gmail.com>
2015-12-07 15:16:05 +0100

    Video 25 - the last - nested routes, using resource

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,40 +3,14 @@
 var App = Ember.Application.create();
 
 App.Router.map(function() {
-	this.route('about', {
-		path: '/about'
+	this.resource('articles', { path: 'articles' }, function() {
+		this.route('new', { path: '/new' });
+		this.route('edit', { path: '/edit/:id' });
 	});
 });
 
 App.IndexRoute = Ember.Route.extend({
 	model: function() {
 		return ['Rojo', 'Amarillo', 'Verde'];
-	},
-	setupController: function(controller, model) {
-		controller.set('model', model);
-	}
-});
-
-App.IndexController = Ember.ArrayController.extend({
-	needs: 'about',
-	actions: {
-		callAboutController: function() {
-			this.get('controllers.about').send('popup');
-		},
-		popup: function() {
-			alert('From Home Page!!!');
-		}
-	}
-});
-
-App.AboutController = Ember.ObjectController.extend({
-	needs: 'index',
-	actions: {
-		callIndexController: function() {
-			this.get('controllers.index').send('popup');
-		},
-		popup: function() {
-			alert('From Abount Page!!!');
-		}
 	}
 });
