aac5946c1be11c25f11b05efd63df624fe4361a8
Eugenio Mendoza Robaina <eu.mendoza@gmail.com>
2015-12-07 15:03:40 +0100

    Video 24 - communicating controllers

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,14 +3,40 @@
 var App = Ember.Application.create();
 
 App.Router.map(function() {
-
+	this.route('about', {
+		path: '/about'
+	});
 });
 
 App.IndexRoute = Ember.Route.extend({
-	model: function(){
+	model: function() {
 		return ['Rojo', 'Amarillo', 'Verde'];
 	},
-	setupController: function(controller, model){
+	setupController: function(controller, model) {
 		controller.set('model', model);
+	}
+});
+
+App.IndexController = Ember.ArrayController.extend({
+	needs: 'about',
+	actions: {
+		callAboutController: function() {
+			this.get('controllers.about').send('popup');
+		},
+		popup: function() {
+			alert('From Home Page!!!');
+		}
+	}
+});
+
+App.AboutController = Ember.ObjectController.extend({
+	needs: 'index',
+	actions: {
+		callIndexController: function() {
+			this.get('controllers.index').send('popup');
+		},
+		popup: function() {
+			alert('From Abount Page!!!');
+		}
 	}
 });
